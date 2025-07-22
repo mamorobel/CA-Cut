86f5a998e4b630b70ab303e6613872a6d6a8741c
mamorobel <robelanbessa86@gmail.com>
2025-07-22 17:12:32 -0400

    readme: setup section

--- tools/setup.sh
+++ tools/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Creating conda environment and installing requirments as defined in ../configurations/environment.yml"
-conda env create -f ../environment.yml
+conda env create -f ../configurations/environment.yml
 
 echo "Creating all necessary directories"
 mkdir ../checkpoints ../data ../plots
