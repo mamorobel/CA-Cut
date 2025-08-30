879cb89d6da334a57bb12feb9a8516075e488742
mamorobel <95201389+mamorobel@users.noreply.github.com>
2025-08-29 21:42:36 -0400

    Delete tools/setup.bat

--- tools/setup.bat
+++
@@ -1,7 +0,0 @@
-echo Creating all necessary directories
-mkdir ..\checkpoints
-mkdir ..\data
-mkdir ..\plots
-
-echo Creating conda environment and installing requirments as defined in ../configurations/environment.yml
-conda env create -f ../configurations/environment.yml
